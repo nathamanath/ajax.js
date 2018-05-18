14068e2c7ab3c08a8d9a6481db6ac1f2b9f71d18
NathanG <nathan@monochrome.co.uk>
2018-05-18 11:22:39 +0100

    open xhr before setting responseType

--- dist/ajax.js
+++ dist/ajax.js
@@ -381,9 +381,9 @@ exports.default = {
 
     // ie >= 10 and browsers
     if ('withCredentials' in xhr) {
-      xhr.responseType = args.responseType;
-
       openXhr(xhr, method, url);
+
+      xhr.responseType = args.responseType;
       setHeaders(xhr, args.headers);
       bindStandardEvents(xhr, args);
 
